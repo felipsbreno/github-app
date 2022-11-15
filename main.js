e1e19d7d8f0440d1ee40a579612235b7cccd7ebf
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-15 13:40:10 -0300

    deixando as funçoes fora do scopo $

--- main.js
+++ main.js
@@ -4,38 +4,38 @@ $(function () {
 });
 
 $(function () {
-  function runEffect() {
-    var selectedEffect = $('#effectTypes').val();
-
-    var options = {};
-    if (selectedEffect === 'scale') {
-      options = { percent: 50 };
-    } else if (selectedEffect === 'size') {
-      options = { to: { width: 200, height: 60 } };
-    }
-
-    $('#effect').toggle(selectedEffect, options, 500);
-  }
-
   $('#button').on('click', function () {
     runEffect();
   });
 });
 
 $(function () {
-  function getApi() {
-    var username = $('#github').val();
-
-    $('#form').submit(function (event) {
-      event.preventDefault();
-    });
-
-    $.get('https://api.github.com/users/' + username, function (result) {
-      console.log(result);
-    });
-  }
-
   $('#search-btn').on('click', function () {
     getApi();
   });
 });
+
+function runEffect() {
+  var selectedEffect = $('#effectTypes').val();
+
+  var options = {};
+  if (selectedEffect === 'scale') {
+    options = { percent: 50 };
+  } else if (selectedEffect === 'size') {
+    options = { to: { width: 200, height: 60 } };
+  }
+
+  $('#effect').toggle(selectedEffect, options, 500);
+}
+
+function getApi() {
+  var username = $('#github').val();
+
+  $('#form').submit(function (event) {
+    event.preventDefault();
+  });
+
+  $.get('https://api.github.com/users/' + username, function (result) {
+    console.log(result);
+  });
+}
