42b7332f18fcfd24a6ede153c636985aa0314cb1
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-15 11:18:39 -0300

    adicionando alguns efeitos com jquery

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $(function () {
+  $('#effect').addClass('hide');
   $('#tabs').tabs();
+});
+
+$(function () {
+  function runEffect() {
+    var selectedEffect = $('#effectTypes').val();
+
+    var options = {};
+    if (selectedEffect === 'scale') {
+      options = { percent: 50 };
+    } else if (selectedEffect === 'size') {
+      options = { to: { width: 200, height: 60 } };
+    }
+
+    $('#effect').toggle(selectedEffect, options, 500);
+  }
+
+  $('#button').on('click', function () {
+    runEffect();
+  });
 });
